79be4cfd49880231a845427a420f0cf8b4d85f47
Kevin <kevin.ongko@gmail.com>
2024-07-15 10:35:42 +0700

    update

---
+++ after/ftplugin/php.lua
@@ -0,0 +1,3 @@
+vim.bo.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
+vim.bo.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+vim.bo.expandtab = true -- Use spaces instead of tabs
--- lua/kevin/plugins/nvim-tree.lua
+++ lua/kevin/plugins/nvim-tree.lua
@@ -5,15 +5,48 @@ return {
     local nvimtree = require("nvim-tree")
 
     nvimtree.setup({
+      filters = { custom = { "^.git$" } },
       view = {
-        width = 30,
-        relativenumber = true,
+        adaptive_size = true,
+        relativenumber = true
+      },
+      git = {
+        ignore = false
       }
     })
 
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+
+    -- auto close
+    vim.api.nvim_create_autocmd("QuitPre", {
+      callback = function()
+        local tree_wins = {}
+        local floating_wins = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(tree_wins, w)
+          end
+          if vim.api.nvim_win_get_config(w).relative ~= '' then
+            table.insert(floating_wins, w)
+          end
+        end
+        if 1 == #wins - #floating_wins - #tree_wins then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(tree_wins) do
+            vim.api.nvim_win_close(w, true)
+          end
+        end
+      end
+    })
+
+    local function open_nvim_tree()
+      require("nvim-tree.api").tree.open()
+    end
+    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
     -- set keymaps
     local keymap = vim.keymap
