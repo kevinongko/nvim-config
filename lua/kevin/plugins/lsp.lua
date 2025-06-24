78a46ea4faac728bec927c36175767e0a3f6c20a
Kevin Ongko <kevin.ongko>
2025-06-24 11:59:17 +0700

    update

--- lua/kevin/plugins/lsp.lua
+++ lua/kevin/plugins/lsp.lua
@@ -1,59 +1,59 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
 
-		-- Basic LSP capabilities
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- Basic LSP capabilities
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		vim.diagnostic.config({
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = signs.Error,
-					[vim.diagnostic.severity.WARN] = signs.Warn,
-					[vim.diagnostic.severity.HINT] = signs.Hint,
-					[vim.diagnostic.severity.INFO] = signs.Info,
-				},
-			},
-		})
+    -- Change the Diagnostic symbols in the sign column (gutter)
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = signs.Error,
+          [vim.diagnostic.severity.WARN] = signs.Warn,
+          [vim.diagnostic.severity.HINT] = signs.Hint,
+          [vim.diagnostic.severity.INFO] = signs.Info,
+        },
+      },
+    })
 
-		local vue_language_server_path = vim.fn.expand("$MASON/packages")
-			.. "/vue-language-server"
-			.. "/node_modules/@vue/language-server"
+    local vue_language_server_path = vim.fn.expand("$MASON/packages")
+        .. "/vue-language-server"
+        .. "/node_modules/@vue/language-server"
 
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-			init_options = {
-				plugins = {
-					{
-						name = "@vue/typescript-plugin",
-						location = vue_language_server_path,
-						languages = { "vue" },
-					},
-				},
-			},
-			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-		})
-		lspconfig.volar.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-	end,
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+          },
+        },
+      },
+      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    })
+    lspconfig.volar.setup({
+      capabilities = capabilities,
+    })
+  end,
 }
--- lua/kevin/plugins/mason.lua
+++ lua/kevin/plugins/mason.lua
@@ -1,47 +1,48 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"html",
-				"cssls",
-				"lua_ls",
-				"ts_ls",
-				"vue_ls",
-				"rust_analyzer",
-			},
-		})
+    mason_lspconfig.setup({
+      automatic_enable = false,
+      ensure_installed = {
+        "html",
+        "cssls",
+        "lua_ls",
+        "ts_ls",
+        "vue_ls",
+        "rust_analyzer",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"oxlint",
-				"eslint_d",
-				"prettierd",
-				"stylua",
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "oxlint",
+        "eslint_d",
+        "prettierd",
+        "stylua",
+      },
+    })
+  end,
 }
